63028682cfc79dcd17ca6d7baf417d9b39c5909f
Jonathan <ing.jonathrodribarra@gmail.com>
2026-02-14 00:08:03 -0600

    Create variables_basicas.cpp
    Add basic variables example in C++

---
+++ 01_variables/variables_basicas.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Programa básico para practicar variables
+int main() {
+    int edad = 18;
+    float altura = 1.60;
+    char inicial = 'J';
+    string nombre = "Jonathan";
+
+    cout << "Nombre: " << nombre << endl;
+    cout << "Edad: " << edad << endl;
+    cout << "Altura: " << altura << endl;
+    cout << "Inicial: " << inicial << endl;
+
+    return 0;
+}
